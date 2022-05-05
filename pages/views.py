4166491d38c22666bfe57ddfc978a4d6983cd4ab
martinBmeza <camartinbu@gmail.com>
2022-05-05 09:48:56 -0300

    restore gitignore

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+"""
+Usando function-based
 def homePageView(request):
-    return HttpResponse('A la grande le puse cuca!')
-# Create your views here.
+    return render(request, 'home.html') #devolviendo un template estatico
+    #return HttpResponse('A la grande le puse cuca!') #devolviendo una cadena plana
+"""
+
+#usando class based 
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+    
+class AboutPageView(TemplateView):
+    template_name = "about.html"

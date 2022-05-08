6e1d68b23eb38f38e7c172386d47d723069eddc0
martinBmeza <camartinbu@gmail.com>
2022-05-08 12:53:17 -0300

    unit tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Post
 
-# Create your tests here.
+class PostTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        #Con esto creo unos datos para hacerle pruebas
+        #uso el modelo que defini, que es el que quiero testear
+        cls.post = Post.objects.create(text="This is a test")
+        
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "This is a test")
+    
+    def test_url_exist_at_correct_location(self):
+        response = self.client.get("/home2/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage(self):
+        response = self.client.get(reverse("home2"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home2.html")
+        self.assertContains(response, "This is a test")
